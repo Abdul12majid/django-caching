780720e6615ce9f79e00c64e4ce59992a9d78a39
Abdul12majid <yisaabdulmajid@gmail.com>
2024-12-02 16:14:16 +0100

    created/register model

--- cache_app/models.py
+++ cache_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=50, blank=False, null=False)
+	serial_number = models.UUIDField(default=uuid.uuid4, editable=False)
+	
+	def __str__(self):
+		return self.name + ' ' + self.serial_number
