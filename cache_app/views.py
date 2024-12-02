8e5b6fbe25566afe648e822d6b1f2cbf391d982f
Abdul12majid <yisaabdulmajid@gmail.com>
2024-12-02 16:27:39 +0100

    display product view

--- cache_app/views.py
+++ cache_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ShowProduct
+from rest_framework.generics import ListCreateAPIView
+
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Caching")
+
+class products(ListCreateAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ShowProduct
