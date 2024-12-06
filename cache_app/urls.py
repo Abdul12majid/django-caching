c6f5f4baad9325906e8dc68c7f4c53663200ceac
Abdul12majid <yisaabdulmajid@gmail.com>
2024-12-06 15:59:04 +0100

    created cached view/url, added cache decorator

--- cache_app/urls.py
+++ cache_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name="index"),
     path('products/', views.products.as_view()),
+    path('cached/', views.cached, name="cached"),
 ]
--- cache_app/views.py
+++ cache_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ShowProduct
 from rest_framework.generics import ListCreateAPIView
+from django.views.decorators.cache imort cache_page
 
 
 # Create your views here.
@@ -14,3 +15,10 @@ def index(request):
 class products(ListCreateAPIView):
 	queryset = Product.objects.all()
 	serializer_class = ShowProduct
+
+
+@cache_page(60*15)
+def cached(request):
+	all_produts = Product.objects.all()
+	serializer = ShowProduct(all_produts, many=True)
+	return Response({'products':serializer.data})
